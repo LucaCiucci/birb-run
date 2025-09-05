bae8cda40da766a866aa37f26168d6bc21c48b5d
Luca Ciucci <luca.ciucci99@gmail.com>
2025-09-05 15:54:24 +0200

    handle shebang

--- src/run/execution/naive.rs
+++ src/run/execution/naive.rs
@@ -1,6 +1,9 @@
-use std::{borrow::Borrow, io::{BufRead}, path::Path};
+use std::io::Write;
+use std::{borrow::Borrow, io::BufRead, path::Path};
 use std::sync::mpsc;
 use std::thread;
+
+use tempfile::NamedTempFile;
 
 use crate::{command::Command, run::execution::CommandExecutor};
 
@@ -26,9 +29,26 @@ impl<F: FnMut(&str)> CommandExecutor for NaiveExecutor<F> {
 
 impl<F: FnMut(&str)> NaiveExecutor<F> {
     fn exec_shell(pwd: impl AsRef<Path>, cmd: &str, mut output_handler: impl FnMut(&str)) -> anyhow::Result<()> {
-        let mut child = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(cmd)
+        // try to find the shebang
+        let shebang = cmd.lines().next().filter(|line| line.starts_with("#!")).map(|line| line.to_string());
+        let mut script: NamedTempFile;
+        let (program, args) = if let Some(shebang) = shebang {
+            let interpreter = shebang.trim_start_matches("#!").trim();
+            let mut args = shlex::split(interpreter).expect("Failed to parse shebang");
+            assert!(!args.is_empty(), "Shebang must contain at least the interpreter");
+            let program = args.remove(0);
+            script = NamedTempFile::new().expect("Failed to create temporary file");
+            script.write_all(cmd.as_bytes()).expect("Failed to write to temporary file");
+            args.push(script.path().to_string_lossy().to_string());
+            (program, args)
+        } else {
+            ("sh".to_string(), vec!["-c".to_string(), cmd.to_string()]) // TODO avoid useless string clone, use cow or something
+        };
+
+        eprintln!("Executing command: {} {:?}", program, args);
+
+        let mut child = std::process::Command::new(program)
+            .args(args)
             .current_dir(pwd)
             .stdout(std::process::Stdio::piped())
             .stderr(std::process::Stdio::piped())
